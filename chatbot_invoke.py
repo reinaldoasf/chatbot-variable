dfc371b1d71da1594b92c9d2803637e5b2a75719
reinaldoasf <reinaldoasf@gmail.com>
2025-07-29 13:39:14 -0300

    Modified version of chat to include context instead of query response mechanism

--- chatbot_invoke.py
+++ chatbot_invoke.py
@@ -20,15 +20,34 @@ def main():
     index = load_index_from_storage(storage_context)
     print("Knowledge Base loaded successfully!")
 
-    query_engine = index.as_query_engine(similarity_top_k=5, streaming=True)
+    #The follow line made a simple query response chat search information over knowledegebase
+    #replace chat engine if it is the desired behavior
+    #query_engine = index.as_query_engine(similarity_top_k=5, stream=True)
+    chat_engine = index.as_chat_engine(
+        chat_mode="context",
+        similarity_top_k=5,
+        system_prompt=(
+            """Você é um agente de busca de informação
+                Responda somente com a informação adquirida.
+                """
+        )
+    )
     
     while True:
-        query = input("Digite sua pergunta(Ou digite q para sair):")
+        query = input("Digite sua pergunta(Ou digite q para sair ou /reset para limpar o histórico da conversa):")
         if query.lower() == 'q':
             break
-        response = query_engine.query(query)
-        print("Resposta: ", end="")
-        response.print_response_stream()
+        if query.lower() == '/reset':
+            chat_engine.reset()
+            print("\n-------- Limpado historico de conversa -------\n")
+            continue
+        response_stream = chat_engine.stream_chat(query)
+        print("Resposta do chat: ", end="")
+        for token in response_stream.response_gen:
+            print(token, end="")
+        #response = query_engine.query(query)
+        #print("Resposta: ", end="")
+        #response.print_response_stream()
         print("\n\n"+'-'*50+"\n\n")
     
     print("Obrigado por interagir com nosso chat.")
